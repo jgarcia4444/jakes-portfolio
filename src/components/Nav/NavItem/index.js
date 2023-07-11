760c54ff107227bc82884c87eb163b10daf84659
jgarcia4444 <bballwiz4@gmail.com>
2023-07-10 20:18:05 -0600

    Added in a copyright on the bottom left of the website sitting on top of the nav.

--- src/components/Nav/NavItem/index.js
+++ src/components/Nav/NavItem/index.js
@@ -1,15 +1,17 @@
 import React from 'react'
 
-const NavItem = ({text, icon}) => {
+const NavItem = ({itemInfo}) => {
+
+    const {text, icon, to} = itemInfo;
 
     console.log(icon);
     return (
-        <div className={`flex cursor-pointer flex-row justify-start mb-6 items-center`}>
-            {icon}
-            <p className="ml-2 font-bold">
-                {text}
-            </p>
-        </div> 
+        <a href={to} className={`flex cursor-pointer flex-row justify-start mb-6 items-center`}>
+                {icon}
+                <p className="ml-2 font-bold">
+                    {text}
+                </p>
+        </a> 
     )
 };
 
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -8,44 +8,49 @@ const Nav = () => {
 
     const links = [
         {
-            to: "", 
+            to: "/", 
             text: "Home", 
             icon: <FiHome color='#fff' size={22} />
         },
         {
-            to: "", 
+            to: "/about", 
             text: "About", 
             icon: <FiUser color="#fff" size={22} />
         }, 
         {
-            to: "", 
+            to: "/portfolio", 
             text: "Portfolio", 
             icon: <FiBookOpen color="#fff" size={22} />
         }, 
         {
-            to: "", 
+            to: "/services", 
             text: "Services", 
             icon: <FiServer color="#fff" size={22} />
         }, 
         {
-            to: "", 
+            to: "/contact", 
             text: "Contact", 
             icon: <FiMail color={"#fff"} size={22} />
         }, 
     ];
 
     const renderLinks = () => {
-        return links.map((link, i) => <NavItem icon={link.icon} text={link.text} key={`${i}-${link.text}`} /> );
+        return links.map((link, i) => <NavItem itemInfo={link} key={`${i}-${link.text}`} /> );
     }
 
     return (
-        <div className="w-1/5 flex flex-col h-screen bg-black bg-opacity-80 py-4 px-6">
+        <div className="w-1/5 flex flex-col h-screen bg-black bg-opacity-80 py-4 px-6 justify-between">
+            <div className="w-full flex flex-col">
                 <Headshot />
                 <div className="w-full flex flex-row justify-center mt-2">
                     <h2 className="font-bold text-2xl">Jake Garcia</h2>
                 </div>
-            <div className="w-full flex flex-col mt-12 ">
-                {renderLinks()}
+                <div className="w-full flex flex-col mt-12 ">
+                    {renderLinks()}
+                </div>
+            </div>
+            <div className="w-full">
+                <p className="text-center font-thin text-gray-500">&copy;2023 Jake Garcia</p>
             </div>
         </div>
     )
